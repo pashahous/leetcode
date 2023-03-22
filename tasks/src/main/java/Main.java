f665301cbd40fa5115333e6422bfddf66e414529
Pavel Yushchenkov <pashahous1@rambler.ru>
2023-03-22 22:44:14 +0300

    1480. Running Sum of 1d Array

--- tasks/src/main/java/Main.java
+++ tasks/src/main/java/Main.java
@@ -1,5 +1,23 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test com");
+        System.out.println(Arrays.toString(runningSum(new int[]{1, 2, 3, 4})));
+    }
+
+
+    public static int[] runningSum(int[] nums) {
+        /*1480. Running Sum of 1d Array
+        Input: nums = [1,2,3,4]
+        Output: [1,3,6,10]
+        Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+         */
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j <=i; j++) {
+                result[i] += nums[j];
+            }
+        }
+        return  result;
     }
 }
